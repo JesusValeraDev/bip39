ff586dbfd8903da3e3baf0d341a6739ac1b5fb95
JesusValera <jesus1902@outlook.com>
2025-10-18 02:48:02 +0200

    Add coverage for scrutinizer

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     ],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'clover'],
       exclude: [
         'node_modules/',
         'test/',
